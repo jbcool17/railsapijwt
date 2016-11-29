d3a643601e2b4d24ce89fb2310bec0ab7dab5ca5
John Brilla <jbrilla17@gmail.com>
2016-11-29 05:27:31 -0500

    setup API with JWT Token User Post Signup User Post Login to get JWT setup beer api setup html with ajax test in public folder

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  resources :users, only: :create do
+    collection do
+      post 'confirm'
+      post 'login'
+    end
+  end
+
+  resources :beers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
