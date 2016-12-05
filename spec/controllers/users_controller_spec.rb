df754a683a1b2d9b6fde3a6d7d88d13b552ef07b
John Brilla <jbrilla17@gmail.com>
2016-12-05 14:08:03 -0500

    add user create tests notes - factory girl needs setup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,45 @@
 require 'spec_helper'
 
 describe UsersController do
-  describe "GET #index" do
-    it "populates an array of contacts"
-    it "renders the :index view"
+  describe "POST #create - USER" do
+    before(:each) do
+      @user_good = {email: 'johntest@user.com', password: 'test123', password_confirmation: 'test123'}
+      @user_bad_password = {email: 'johntest@user.com', password: 'test123', password_confirmation: 'test12'}
+      @user_bad_email= {email: '', password: 'test123', password_confirmation: 'test123'}
+    end
+
+    it "get 201 status" do
+      get :create, method: :post, params: {user: @user_good}
+
+      expect(response).to have_http_status(201)
+    end
+
+    it "USER create successful message" do
+      get :create, method: :post, params: {user: @user_good}
+
+      json = JSON.parse(response.body)
+      expect(json['status']).to eq("User created successfully")
+    end
+
+    it "USER create unsuccessful message - No Email" do
+      get :create, method: :post, params: {user: @user_bad_email}
+
+      json = JSON.parse(response.body)
+      expect(json['errors'].first).to eq("Email can't be blank")
+    end
+
+    it "USER create unsuccessful message - No Password" do
+      get :create, method: :post, params: {user: {email: 'test@user.com', password_confirmation:'test123'}}
+
+      json = JSON.parse(response.body)
+      expect(json['errors'].first).to eq("Password can't be blank")
+    end
+
+    it "USER create unsuccessful message - Password Confirmation" do
+      get :create, method: :post, params: {user: @user_bad_password}
+
+      json = JSON.parse(response.body)
+      expect(json['errors'].first).to eq("Password confirmation doesn't match Password")
+    end
   end
 end
